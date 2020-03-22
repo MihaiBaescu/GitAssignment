0b1b7fa4028ab86a6ca9079df7b26c2270eb0d03
Mihai <Mihai@DESKTOP-BENF7R9.local>
2020-03-22 16:24:09 +0200

    Am creat clasa "Utilizator" cu atributele "kilograme" si "inaltime"

---
+++ Tema2CTS/src/Utilizator.java
@@ -0,0 +1,12 @@
+
+public class Utilizator {
+
+	private float kilograme;
+	private float inaltime;
+	
+	public Utilizator(float kilograme, float inaltime) {
+		
+		this.kilograme = kilograme;
+		this.inaltime = inaltime;
+	}
+}
